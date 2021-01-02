b658acbbc99142f030deba86a0ade12cc7b497e7
Finn Thye <piqueen@gmail.com>
2021-01-02 11:13:48 -0700

    Create pages for Chico and Sacramento

---
+++ src/pages/chico.js
@@ -0,0 +1,52 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+// markup
+const ChicoPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Chico</title>
+      <h1 style={headingStyles}>
+        Welcome to Chico California!
+        <br />
+        <span style={headingAccentStyles}>— Super fun stuff is coming! </span>
+        <span role="img" aria-label="Party popper emojis">
+          🎉🎉🎉
+        </span>
+      </h1>
+      <p style={paragraphStyles}>
+        Edit <code style={codeStyles}>src/pages/chico.js</code> to see this page
+        update in real-time.{" "}
+        <span role="img" aria-label="Sunglasses smiley emoji">
+          😎
+        </span>
+      </p>
+    </main>
+  )
+}
+
+export default ChicoPage
